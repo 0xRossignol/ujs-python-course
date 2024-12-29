4007e4d7a5fc172072f73ab3889193f9efc06a52
rossignol <2746054243@qq.com>
2024-12-29 16:48:36 +0800

    修复了英语单词单复数同时出现的bug

--- pyLab/design/modules/crawler.py
+++ pyLab/design/modules/crawler.py
@@ -17,7 +17,8 @@ class Crawler:
             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
             "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 "
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X)"
+            " AppleWebKit/605.1.15 "
             "(KHTML, like Gecko) Version/14.1 Mobile/15E148 Safari/604.1"
         ]
         self.headers = headers if headers else {}
